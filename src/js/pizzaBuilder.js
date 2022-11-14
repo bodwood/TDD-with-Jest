a690a38f71732a1155aaa44cbe3cbe59e2760a9d
bodwood <bodiewood@hotmail.com>
2022-11-13 22:01:23 -0800

    add constructor called Pizza and addToppings prototype function

--- src/js/pizzaBuilder.js
+++ src/js/pizzaBuilder.js
@@ -1,2 +1,15 @@
 //Business Logic
+export default class Pizza {
+  constructor(topping1, topping2, topping3){
+    this.topping1 = topping1;
+    this.topping2 = topping2;
+    this.topping3 = topping3;
+  } addToppings(){
 
+  }
+}
+
+window.addEventListener("load", function() {
+  const addBtn = document.getElementById('addBtn');
+  addBtn.addEventListener('click', addToppings);
+})
